f593d11e1c6dc90e9987b05e38b1dbdc472f54a3
zakuzakukakigori <zakuzakukakigori@gmail.com>
2024-07-20 07:36:44 +0900

    異なる出力方法を試す

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-    println!("rect1 is {:#?}", rect1);
+    dbg!(&rect1);
 }
