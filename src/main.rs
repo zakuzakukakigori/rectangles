1422d5ee8cf7f0d6637e5e481358acc18482e448
zakuzakukakigori <zakuzakukakigori@gmail.com>
2024-07-20 08:43:37 +0900

    square関連関数を定義する

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
@@ -30,4 +39,7 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    dbg!(sq.area());
 }
