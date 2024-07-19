79233214f85fafb747e37e74bfaa8ecb9dfcf3d3
zakuzakukakigori <zakuzakukakigori@gmail.com>
2024-07-20 08:15:50 +0900

    フィールドと同じ名前のwidthメソッドを定義する

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+    fn width(&self) -> bool {
+        self.width > 0
     }
 }
 
@@ -16,8 +16,7 @@ fn main() {
         height: 50,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
