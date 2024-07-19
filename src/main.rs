0d833e9030dbce7e362af3f7fbff69ace3b6b4eb
zakuzakukakigori <zakuzakukakigori@gmail.com>
2024-07-20 07:44:24 +0900

    Rectangle構造体上にareaメソッドを定義する

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
-    let scale = 2;
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
-    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
